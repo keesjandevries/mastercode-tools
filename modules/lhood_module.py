75e0cc9a1f77b1eab6f496d59db948e8b90d205c
Sam Rogerson <samr@cern.ch>
2012-02-21 16:54:00 +0100

    removed standard filename values

--- modules/lhood_module.py
+++ modules/lhood_module.py
@@ -4,13 +4,15 @@ from ctypes import cdll
 from ctypes import byref
 from ctypes import c_double
 from ctypes import c_int
-lhoodLib = cdll.LoadLibrary('./libs/libLH.so')
+lhoodLib = cdll.LoadLibrary('../reprocessing/libs/libLH.so')
+
+contour_base_dir = '../reprocessing/lhoods/contour_lookups/'
+lh1d_base_dir = '../reprocessing/lhoods/1d_lookups/'
 
 class ContourLikelihood( object ):
     def __init__(self, filename="", chi2 = 5.99, chi2_inf = 0.0) :
         chi2_c     = c_double( chi2 )
         chi2_inf_c = c_double( chi2_inf )
-        filename = str('lhoods/lookups/contour_lookups/mc7/cms_1.1fb_2011.csv')
         self.obj = lhoodLib.ContourLikelihood_new( str(filename), byref(chi2_c),
                                                    byref(chi2_inf_c) )
     def getChi2( self, x, y ) :
@@ -22,7 +24,6 @@ class ContourLikelihood( object ):
 
 class Likelihood1D( object ) :
     def __init__(self, filename="", function = 6, mu = 0., sigma = 0., ndof = 1 ) :
-        filename = 'lhoods/lookups/1d_lookups/mc7/bs_cms_lhcb_s_sb.dat'
         function_c = c_int( function )
         mu_c = c_double( mu )
         sigma_c = c_double( sigma )
