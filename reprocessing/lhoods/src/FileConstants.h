d7f242d179c7ab710f4789b8f4972a4341fdb160
Sam Rogerson <samr@cern.ch>
2012-02-22 18:41:56 +0100

    updated locations

--- reprocessing/lhoods/src/FileConstants.h
+++ reprocessing/lhoods/src/FileConstants.h
@@ -8,33 +8,33 @@
  */
 /* CONTOURS */
 // MC7
-const std::string cmsATeps("/home/hep/sr505/mastercode-tools/contour_lookups/mc7/cms_1.1fb_2011.csv");
-const std::string atlas0leps("/home/hep/sr505/mastercode-tools/contour_lookups/mc7/atlas_0l_1040pb.csv");
+const std::string cmsATeps("reprocessing/lhoods/lookups/contour_lookups/mc7/cms_1.1fb_2011.csv");
+const std::string atlas0leps("reprocessing/lhoods/lookups/contour_lookups/mc7/atlas_0l_1040pb.csv");
 // MC6
-const std::string cms_new_at("/home/hep/sr505/mastercode-tools/contour_lookups/mc6/cms_new_at.txt");
-const std::string atlas_combined("/home/hep/sr505/mastercode-tools/contour_lookups/mc6/atlas_combined.txt");
+const std::string cms_new_at("reprocessing/lhoods/lookups/contour_lookups/mc6/cms_new_at.txt");
+const std::string atlas_combined("reprocessing/lhoods/lookups/contour_lookups/mc6/atlas_combined.txt");
 // MC5
-const std::string cms_nlo_expected("/home/hep/sr505/mastercode-tools/contour_lookups/cms_nlo_expected.txt");
-const std::string cms_nlo_observed("/home/hep/sr505/mastercode-tools/contour_lookups/cms_nlo_observed.txt");
-const std::string atlas_1l_observed("/home/hep/sr505/mastercode-tools/contour_lookups/atlas-1l_observed.txt");
-const std::string atlas_1l_expected("/home/hep/sr505/mastercode-tools/contour_lookups/atlas-1l_expected.txt");
-const std::string atlas0l_c1("/home/hep/sr505/mastercode-tools/contour_lookups/atlas-0l_observed.txt");
-const std::string atlas0l_c2("/home/hep/sr505/mastercode-tools/contour_lookups/atlas-0l_expected.txt");
+const std::string cms_nlo_expected("reprocessing/lhoods/lookups/contour_lookups/cms_nlo_expected.txt");
+const std::string cms_nlo_observed("reprocessing/lhoods/lookups/contour_lookups/cms_nlo_observed.txt");
+const std::string atlas_1l_observed("reprocessing/lhoods/lookups/contour_lookups/atlas-1l_observed.txt");
+const std::string atlas_1l_expected("reprocessing/lhoods/lookups/contour_lookups/atlas-1l_expected.txt");
+const std::string atlas0l_c1("reprocessing/lhoods/lookups/contour_lookups/atlas-0l_observed.txt");
+const std::string atlas0l_c2("reprocessing/lhoods/lookups/contour_lookups/atlas-0l_expected.txt");
 
 /* 1D Lookups */
 // MC7
-const std::string Bsmumu_LHCb_CMS_CDF_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc7/LHCb_CMS_CDF.dat");
-const std::string Bsmumu_LHCb_CMS_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc7/bs_cms_lhcb_s_sb.dat");
-const std::string HAtautauEPS_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc7/HA-tautau-eps.csv");
-const std::string HAtautau_16_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc7/Htt-CMS-1.6fb.csv");
+const std::string Bsmumu_LHCb_CMS_CDF_file("reprocessing/lhoods/lookups/1d_lookups/mc7/LHCb_CMS_CDF.dat");
+const std::string Bsmumu_LHCb_CMS_file("reprocessing/lhoods/lookups/1d_lookups/mc7/bs_cms_lhcb_s_sb.dat");
+const std::string HAtautauEPS_file("reprocessing/lhoods/lookups/1d_lookups/mc7/HA-tautau-eps.csv");
+const std::string HAtautau_16_file("reprocessing/lhoods/lookups/1d_lookups/mc7/Htt-CMS-1.6fb.csv");
 // MC6
-const std::string Bsmumu_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc6/br_bsmumu_enlarged.dat");
-const std::string XenonSignal_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc6/xenonSignal_enlarged.dat");
-const std::string XenonContour_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc6/xenon_contour.csv");
-const std::string HAtautau_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc6/HA-tautau-contour.csv");
-const std::string HA_sigbr68_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc6/sig-br-68.csv");
-const std::string HA_sigbr95_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc6/sig-br-95.csv");
-const std::string HA_sigbr997_file("/home/hep/sr505/mastercode-tools/1d_lookups/mc6/sig-br-99-7.csv");
+const std::string Bsmumu_file("reprocessing/lhoods/lookups/1d_lookups/mc6/br_bsmumu_enlarged.dat");
+const std::string XenonSignal_file("reprocessing/lhoods/lookups/1d_lookups/mc6/xenonSignal_enlarged.dat");
+const std::string XenonContour_file("reprocessing/lhoods/lookups/1d_lookups/mc6/xenon_contour.csv");
+const std::string HAtautau_file("reprocessing/lhoods/lookups/1d_lookups/mc6/HA-tautau-contour.csv");
+const std::string HA_sigbr68_file("reprocessing/lhoods/lookups/1d_lookups/mc6/sig-br-68.csv");
+const std::string HA_sigbr95_file("reprocessing/lhoods/lookups/1d_lookups/mc6/sig-br-95.csv");
+const std::string HA_sigbr997_file("reprocessing/lhoods/lookups/1d_lookups/mc6/sig-br-99-7.csv");
 
 
 /*
