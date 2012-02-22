e1e3047544847acf8e59d067a8bb201707966592
Sam Rogerson <samr@cern.ch>
2012-02-22 18:43:27 +0100

    finished adding in constructor arguments

--- modules/lhood_dict.py
+++ modules/lhood_dict.py
@@ -5,11 +5,16 @@ def get_lhood_dict() :
     CONT  = c_type[0]
     LH1D  = c_type[1]
 
+    # this needs to be done better than it is; but it's working around a silly implementation of LHoods
     # name; argument to be blotted to constructor, axis title for fun
-    d = { 
-          "CMSalphaT1.6"  : [ CONT, [ "reprocessing/lhoods/lookups/contour_lookups/mc7/cms_1.1fb_2011.csv", 5.99, 0.0 ], 
+    d = {
+          "CMSalphaT1.6"  : [ CONT, [ "reprocessing/lhoods/lookups/contour_lookups/mc7/cms_1.1fb_2011.csv", 5.99, 0.0 ],
                               "CMS #alpha_{T} 1.6fb^{-1}" ],
-          "BsmumuLHCbCMS" : [ LH1D, [ "./reprocessing/lhoods/lookups/1d_lookups/mc7/bs_cms_lhcb_s_sb.dat" ],             
+          "BsmumuLHCbCMS" : [ LH1D, [ "reprocessing/lhoods/lookups/1d_lookups/mc7/bs_cms_lhcb_s_sb.dat" ],
                               "BR(B_{s}#rightarrow#mu^{+}#mu^{-}) LHCb+CMS" ],
-        }                                                               
+          "Xenon2011"     : [ LH1D, [ "reprocessing/lhoods/lookups/1d_lookups/mc6/xenon_contour.csv", 1, 1.2, 3.2, 1 ],
+                              "Xenon 2011" ],
+          "HA->tt2011"    : [ LH1D, [ "reprocessing/lhoods/lookups/1d_lookups/mc7/Htt-CMS-1.6fb.csv", 9 ],
+                              "CMS H/A #rightarrow#tau#tau 1.6fb^{-1}" ],
+        }
     return d
