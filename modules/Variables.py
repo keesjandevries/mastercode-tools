c9b8d49444d58f8152a8d2ccc7fbd334d0f7092d
Sam Rogerson <samr@cern.ch>
2012-03-29 17:26:23 +0200

    more similar log treament; add update function for Variables: consider linking these classes in a better way

--- modules/Variables.py
+++ modules/Variables.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 
 class Variable(object) : 
-    def __init__(self, index, min_val, max_val, bins = 100, name = "") :
+    def __init__(self, index, min_val, max_val, bins = 100, name = "", log = None) :
         self.index   = index
         self.min_val = min_val 
         self.max_val = max_val
         self.name    = name
         self.bins    = bins
+        if log is not None :
+            self.log = log
 
     def __str__ ( self ) :
         s = ""
@@ -21,16 +23,37 @@ class Variable(object) :
             self.min_val, self.max_val, self.bins )
         return r
 
+    def update( self, d ) :
+        for name, value in d.items() :
+            setattr(self,name,value)
+        return self
+
+    def get_indices( self ) :
+        return [ self.index ]
+
 class Space( object ):
-    def __init__( self, v1, v2, logx = False, logy = False ) :
+    def __init__( self, v1, v2, logx = None, logy = None ) :
         self.xaxis = v1
         self.yaxis = v2
-        self.logx = logx
-        self.logy = logy
-        self.title = "(%s,%s)" % ( v1.name, v2.name )
+        if logx is not None :
+            self.logx = logx
+        elif hasattr(v1,"log") :
+            self.logx = v1.log
+        else :
+            self.logx = False
+        if logy is not None :
+            self.logy = logy
+        elif hasattr(v2,"log") :
+            self.logy = v2.log
+        else :
+            self.logy = False
+
+        self.bins = v1.bins * v2.bins
+
+        self.name = "(%s,%s)" % ( v1.name, v2.name )
 
     def __str__( self ) :
-        return self.title
+        return self.name
         
     def __repr__( self ) :
         r = ""
