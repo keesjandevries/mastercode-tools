70c440e04387f6626d92dc565f2a4a0a2ca1b341
Sam Rogerson <samr@cern.ch>
2012-04-17 17:35:03 +0200

    debug for multimd

--- DataHistos.py
+++ DataHistos.py
@@ -13,7 +13,9 @@ def main( argv=None ) :
         single_hists = hfuncs.get_entry_hist_list( file, files[file], single_vars )
 
         hists = { "pval" : [], "chi2" : [], "dchi" : [] }
-        hfuncs.fill_all_data_hists( file, files[file], space_hists, hists )
+        hfuncs.fill_all_data_hists_nd( file, files[file], space_hists, hists )
+        assert False
+        #hfuncs.fill_all_data_hists( file, files[file], space_hists, hists )
         #hfuncs.fill_all_data_hists_1d( file, files[file], singel_hists, hists )
 
         for hl in hists.values() :
