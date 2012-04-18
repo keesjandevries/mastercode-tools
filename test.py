36d2dd5e516850ae61aec3477ac053910cf904cc
Sam Rogerson <samr@cern.ch>
2012-04-18 14:59:42 +0200

    test new functions

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
 import modules.Variables as v
+import config.plot_list as pl
 
-indices  = [ 10 ]
-min_vals = [ 0 ]
-max_vals = [ 1000 ]
-nbins    = [ 100 ]
-names    = [ "test" ]
-log      = [ False ]
 
-x = v.pSpace( indices, min_vals, max_vals, nbins, names, log )
-
-print repr(x)
+pl.get_plots(10,117)
