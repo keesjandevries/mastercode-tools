4cedee62e10afa00dc85f0373a16eee78f4d2707
Sam Rogerson <samr@cern.ch>
2012-04-20 13:48:18 +0200

    match refactor for recalc

--- RecalculateChi2.py
+++ RecalculateChi2.py
@@ -9,16 +9,17 @@ from modules import recalculate as recalc
 ############################################
 def opts():
     parser = OptionParser("usage: %prog [options]")
-    parser.add_option( "-o", "--output", action="store", dest="output",
-                       default="recalc_out.root", help="name for output file" )
+#    parser.add_option( "-o", "--output", action="store", dest="output",
+#                       default="recalc_out.root", help="name for output file" )
     options,args = parser.parse_args()
     return options, args
 ############################################
 
 def main( argv=None ) :
-    options,args = opts()
-    files = fd.recalc_files()
-    recalc.go( files, options.output )
+#    options,args = opts()
+    file_dict = fd.recalc_files() :
+    for outputfile, opts in file_dict.iteritems() :
+        recalc.go( opts["InputFiles"], outputfile )
 
 if __name__ == "__main__":
     main()
