3efcf8dd1436da717c5d4a74cac267c40eeb5e10
Sam Rogerson <samr@cern.ch>
2012-04-22 14:44:13 +0200

    MCFileCollection class

--- modules/MCFile.py
+++ modules/MCFile.py
@@ -1,10 +1,20 @@
 class MCFile( obj ) :
-    def __init__( d ) :
+    def __init__( self, d ) :
         attrlist = [ "Filename", "Chi2TreeName", "Chi2BranchName", 
                      "ContribTreeName", "ContribBranchName", "PredictionIndex",
-                     "SpectrumIndex", "Inputs", "MinContrib" ]
+                     "SpectrumIndex", "Inputs" ]
         for key in attrlist :  
             val = d.get(key,None)
             if val is None : 
                 print "MCFile Object: %s has not been initialized" % key
             setattr( self,key, d.get(key,None) )
+
+class MCFileCollection( obj ) :
+    def __init__( self, mcfs, d ) :
+        for fpos in range(1, len(self.files) ) :
+            for prop in [ "Chi2BranchName", "ContribBranchName", 
+                          "PredictionIndex", "SpectrumIndex" ] :
+                assert mcfs[fpos].prop == mcfs[0].prop,
+                    "%s does not match across all files in collection" % prop
+        self.files = mcfs
+        print "%d files added to MCFileCollection" % len(self.files)
