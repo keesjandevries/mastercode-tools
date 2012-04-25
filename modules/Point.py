f99e471bb46ace7cb52c900bf557bd34f01eb11a
Keesjan de Vries <keesjan777@gmail.com>
2012-04-25 12:18:09 +0100

    is now able to parse an EntryHisto's name from command line

--- modules/Point.py
+++ modules/Point.py
@@ -1,9 +1,39 @@
+from histogramProcessing import entry_histo_name as histn
+
+
+def HistExists(name):
+    return name
+
 def ParseVar(arg):
     s=arg.split("=")
     var=s[0].replace(' ','')
     val=float(s[1])
     return var, val
 
+def search_hist_name(vars,mcf):
+    vl=[]
+    name="_"
+    for k in vars.keys():
+        vl.append(k)
+    if len(vl)==1:
+        name=histn(vl,mcf)
+        return HistExists(name)
+
+    if len(vl)==2:
+        name=histn(vl,mcf)
+        if HistExists(name):
+            return name
+        else:
+            vlreverse=[vl[1],vl[0]]
+            name=histn(vlreverse,mcf)
+            return HistExists(name)
+
+    else:
+        return 0
+
+
+def GetEntryFromHisto(name,mcf):
+    return 1000
 
 def GetVars(argv):
     # will output something like  {"m0" : 500, "m12" : 1000}
@@ -14,10 +44,13 @@ def GetVars(argv):
     return vars
                         
 def GetEntry(vars,mcf):
-    print vars
+    name=search_hist_name(vars,mcf)
+    print name
+    if name:
+        n=GetEntryFromHisto(name,mcf)
     return 1000
 
 def PrintInfo(n,mcf):
-    print "hello", n, mcf.FileName
+    print  n, mcf.FileName
 
 
