5b7d3ab4bd382e1aaa610f3d4d3dba050e89a251
Sam Rogerson <samr@cern.ch>
2012-05-07 17:05:16 +0200

    update logic to handle nDim

--- modules/point.py
+++ modules/point.py
@@ -1,83 +1,77 @@
 #! /usr/bin/env python
 import ROOT as r
+from itertools import permutations
 
-from histogramProcessing import entry_histo_name as histn
+from histogramProcessing import entry_histo_full_path as histn
 
-def HistExists(name,mcf):
-    f=r.TFile.Open(mcf.FileName)
-    hp=f.Get(name)
-    if hp :
-        f.Close()
-        return name
-    else :
-        f.Close()
-        return 0
+def getVars(argv) :
+    # will output something like  {"m0" : 500, "m12" : 1000}
+    vars={}
+    for arg in argv:
+        var,val=  parseVar(arg)
+        vars[var]=val
+    return vars
 
-def ParseVar(arg):
+def parseVar(arg) :
     s=arg.split("=")
     var=s[0].replace(' ','')
     val=float(s[1])
     return var, val
 
-def search_hist_name(vars,mcf):
-    name="_"
-    vl = vars.keys()
-    if len(vl)==1:
-        name=histn(vl,mcf)
-        return HistExists(name,mcf), vl
-
-    if len(vl)==2:
-        name=histn(vl,mcf)
-        if HistExists(name,mcf):
-            return name, vl
-        else:
-            vl.reverse()
-            name=histn(vl,mcf)
-            return HistExists(name,mcf), vl
-
-    else:
-        print "The given coordinate(s) could not be found in a histogram "
-        return 0
-
-
-def GetEntryFromHisto(vars,order,name,mcf):
-    f=r.TFile.Open(mcf.FileName)
-    hist=f.Get(name).Clone()
-
-    if len(order)==1:
-        var=order[0]
-        bin=hist.FindBin(vars[var])
-        n=hist.GetBinContent(bin)
-
-    if len(order)==2:
-        var1=order[0]
-        var2=order[1]
-        bin=hist.FindBin(vars[var1],vars[var2])
-        n=int(hist.GetBinContent(bin))
-        
-    f.Close()
-
-    return n
-
-def GetVars(argv):
-    # will output something like  {"m0" : 500, "m12" : 1000}
-    vars={}
-    for arg in argv:
-        var,val=  ParseVar(arg)  
-        vars[var]=val
-    return vars
-                        
-def GetEntry(vars,mcf):
-    name,order=search_hist_name(vars,mcf)
+def getEntry(vars,mcf) :
+    name,order=searchHistName(vars,mcf)
 
     if name:
         print "Coordinates found in ", name
-        n=GetEntryFromHisto(vars,order,name,mcf)
-        return n   
+        n=getEntryFromHisto(vars,order,name,mcf)
+        return n
     else :
         return -1
 
-def PrintInfo(n,mcf):
+def getEntryFromHisto(vars,order,name,mcf) :
+    f = r.TFile.Open(mcf.FileName)
+    hist = f.Get(name).Clone()
+    hist.SetDirectory(0)
+    f.Close()
+
+    vals = [ vars[var] for var in order ]
+    print vals
+
+    hBin = hist.FindBin( *vals )
+    n = hist.GetBinContent(hBin)
+    return int(n)
+
+def histExists(name, mcf) :
+    filename = mcf.FileName
+    f = r.TFile(filename)
+    assert not f.IsZombie(), filename
+
+    hOld = f.Get(name)
+    exists = True
+    try :
+        hOld.ClassName()
+    except ReferenceError :
+        exists = False
+    return exists
+
+def listPermutations( l ) :
+    return list( permutations(l) )
+
+def searchHistName(vars,mcf) :
+    var_perms = listPermutations( vars.keys() )
+    h = iter(var_perms)
+
+    hExist = False
+    try :
+        while not hExist :
+            p = h.next()
+            name = histn(p, mcf)
+            hExist = histExists( name, mcf )
+    except StopIteration  :
+        assert False, "The given coordinate(s) could not be found in a histogram "
+    return name, p
+
+def printInfo(n,mcf) :
     print "Found entry number: ", n
 
 
