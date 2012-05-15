5bb4a75234a6fc26e388e58011e675d024bfad4f
Sam Rogerson <samr@cern.ch>
2012-05-15 15:15:01 +0200

    refactor again: take Variable class out

--- modules/MCVariable.py
+++ modules/MCVariable.py
@@ -1,15 +1,17 @@
 from math import sqrt
 from math import fabs
 
-class MCVariable() :
-    def __init__( self, short_name, long_name, constraint_type = None, limit_value = 0, 
-                  errors = [], offset_relative_to = None, index_offset = 0, 
-                  plot_range = (None,None) ) :
+zvalue_functions = { # there might be a better way of doing this... not really sure yet
+    None    : lambda o, x : 0
+    "GAUSS" : lambda o, x : fabs(o.limit_value - x)/o.error
+    "UL"    : lambda o, x : fabs(x - o.limit_value)/o.error if x > o.limit_value else 0.
+    "LL"    : lambda o, x : fabs(o.limit_value - x)/o.error if x < o.limit_value else 0.
+}
 
-        assert offset_relative_to in [ "SpectrumIndex", "PredictionIndex", None ], \
-            "Unkown offset for Variable position" 
-
-        if constraint_type is not None : 
+class Variable() :
+    """ Variable class to insulte from MCVariable becoming obsolete once we move away from trees!"""
+    def __init__( self, short_name, long_name, constraint_type = None, limit_value = 0, errors = [] ) :
+        if constraint_type is not None :
             assert constraint_type in ["GAUSS", "UL", "LL"], \
                 "Unknown constraint type specificed"
             assert len(errors) > 0, "No errors provided for constraint"
@@ -18,24 +20,26 @@ class MCVariable() :
             sq_errors = [ error**2 for error in errors ]
             self.error sqrt( sum(sq_errors) )
 
-        self.zvalue_function = { # there might be a better way of doing this... not really sure yet
-            None    : lambda x : 0
-            "GAUSS" : lambda x : fabs(self.limit_value - x)/self.error
-            "UL"    : lambda x : fabs(x - self.limit_value)/self.error if x > self.limit_value else 0. 
-            "LL"    : lambda x : fabs(self.limit_value - x)/self.error if x < self.limit_value else 0. 
-        }
+        for attr in [ "short_name", "long_name", "constraint_type", "limit_value" ] :
+            setattr( "self.%s" % attr, eval(attr) )
 
-        for attr in [ "short_name", "long_name", "constraint_type", 
-                      "limit_value", "offset_relative_to", "index_offset",
-                      "plot_range" ] :
+    def getZValue(self, value) :
+        return zvalue_function[self.constraint_type]( self, value )
+
+    def getChi2(self, value) :
+        return self.getZValue(value)**2
+
+class MCVariable() :
+    def __init__( self, var, offset_relative_to = None, index_offset = 0,
+                  plot_range = (None,None) ) :
+
+        assert offset_relative_to in [ "SpectrumIndex", "PredictionIndex", None ], \
+            "Unkown offset for Variable position"
+
+        for attr in [ "offset_relative_to", "index_offset", "plot_range" ] :
             setattr( "self.%s" % attr, eval(attr) )
 
     def getIndex(self,mcf) :
         offset = getattr( mcf, self.offset_relative_to, None ) if offset_relative_to is not None else 0
         return ( self.index_offset + offset ) if offset is not None else -1
 
-    def getZValue(self, value) :
-        return self.zvalue_function[self.constraint_type]( value )
-
-    def getChi2(self, value) :
-        return self.getZValue(value)**2
