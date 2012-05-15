4b8ec5ef8cfb2566ff2fe90bf8c8a548c8ac601f
Keesjan de Vries <keesjan777@gmail.com>
2012-05-15 14:48:24 +0100

    added likelihood from ATLAS-CONF-2012-033

--- modules/lhood_dict.py
+++ modules/lhood_dict.py
@@ -8,6 +8,8 @@ def get_lhood_dict() :
     # this needs to be done better than it is; but it's working around a silly implementation of LHoods
     # name; argument to be blotted to constructor, axis title for fun
     d = {
+          "ATLAS_0l_4.7fb"  : [ CONT, [ "reprocessing/lhoods/lookups/contour_lookups/mc8/atlas_0l_5fb.csv", 5.99, 0.0 ],
+                              "ATLAS $4.7FB^{-1}$ ATLAS-CONF-2012-033" ],
           "CMSalphaT1.6"  : [ CONT, [ "reprocessing/lhoods/lookups/contour_lookups/mc7/cms_1.1fb_2011.csv", 5.99, 0.0 ],
                               "CMS #alpha_{T} 1.6fb^{-1}" ],
           "BsmumuLHCbCMS" : [ LH1D, [ "reprocessing/lhoods/lookups/1d_lookups/mc7/bs_cms_lhcb_s_sb.dat" ],
