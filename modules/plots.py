34b1e783a5d76aaac175c63f5a656f167edf5b76
Sam Rogerson <samr@cern.ch>
2012-05-16 16:33:30 +0200

    adapt to new plots style

--- modules/plots.py
+++ modules/plots.py
@@ -1,18 +1,22 @@
-from config import plots
-from modules.Space import Space
-from modules.Space import Contribution
+from config import plots as pc
+from modules.MCSpace import MCSpace
+from modules.MCSpace import MCContribution
+from modules.variables import mc_variables
 
-def standard_plots( pindex, sindex, nbins = 100 ) :
-    v = standard_variables( pindex, sindex, nbins )
-    plots = plots.plots_to_make()
-    l = []
-    for p in plots :
-        l.append([ v[s]+[s] for s in p])
-    spaces = map( make_space, l )
+def get_plots(mcf) :
+    v = mc_variables()
+    plots = pc.plots_to_make()
+    for plane, opts in plots.iteritems() :
+        assert len(plane) == len( opts["ranges"] ) == len( opts["nbins"] ), \
+            "Wrong option numbers for plots (dimensionality doesnt match ranges and/or bins"
+    d = {}
+    for plot_vars, options in plots.iteritems() :
+        d[tuple([ v[var] for var in plot_vars])] = options
 
+    spaces = [ MCSpace( mcf, MCV, opts ) for (MCV, opts) in d.iteritems() ]
     return spaces
 
-def standard_contribs( pindex, sindex, ) :
+def get_contribs( pindex, sindex, ) :
     v = standard_variables( pindex, sindex, None )
     c = plots.contributions_to_make()
     contribs = []
@@ -20,11 +24,3 @@ def standard_contribs( pindex, sindex, ) :
         opts = v[contrib]
         contribs.append( Contribution( opts[0], opts[-2], contrib ) )
     return contribs
-
-def make_space( l ) :
-    args = [ [],[],[],[],[],[], [] ]
-    for v in l :
-        for o,a in zip(v,args) :
-            a.append(o)
-    s = Space( *args )
-    return s
