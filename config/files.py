16bbe13a7e69b2d29b342294e059bf26257e59b0
Sam Rogerson <samr@cern.ch>
2012-05-16 16:31:56 +0200

    move onto test file

--- config/files.py
+++ config/files.py
@@ -2,9 +2,9 @@
 import modules.file_dicts as files
 
 def histo_files() :
-#    d = files.cmssm_test_output_files()
+    d = files.cmssm_test_output_files()
 #    d = files.nuhm1_test_output_files()
-    d = files.nuhm1_MCMh_MC7()
+#    d = files.nuhm1_MCMh_MC7()
     return d
 
 def recalc_files() :
