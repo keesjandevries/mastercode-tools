3523838ad8bf62e58c2e9b9d590cd62f5554a3eb
Keesjan de Vries <keesjan777@gmail.com>
2012-05-17 14:41:02 +0100

    change model.lhood to use shortnames as well\!

--- modules/models.py
+++ modules/models.py
@@ -8,6 +8,7 @@ from math import sqrt
 
 def get_lhood_from_file( mcf ) :
     d = ld.get_lhood_dict()
+    mcvars=v.mc_variables()
     out = {}
     filename=getattr( mcf, "LHoodFile", None )
     if filename is not None :
@@ -15,8 +16,7 @@ def get_lhood_from_file( mcf ) :
             for line in f :
                 l = line.split()
                 name = l[0]
-                variables = l[1:]
-                var_ints = [ int(x) for x in variables ]
+                var_ints = [ mcvars[x].getIndex(mcf) for x in l[1:] ]
                 if name in d :
                     out[name] = lhm.LHood( var_ints, d[name] )
     return out
