6c484e0df9f7a7af25bc6981e3189d732b1e1f82
Sam Rogerson <samr@cern.ch>
2012-05-21 13:46:14 +0200

    test histo file

--- config/files.py
+++ config/files.py
@@ -2,7 +2,7 @@
 import modules.file_dicts as files
 
 def histo_files() :
-    d = files.cmssm_test_output_files()
+    d = files.cmssm_test_file_histo()
 #    d = files.nuhm1_test_output_files()
 #    d = files.nuhm1_MCMh_MC7()
     return d
