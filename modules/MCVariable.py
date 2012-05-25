95170d990dbcc5c12951134d1160b15deb459a6d
Keesjan de Vries <keesjan777@gmail.com>
2012-05-25 15:59:07 +0100

    cleanup

--- modules/MCVariable.py
+++ modules/MCVariable.py
@@ -13,33 +13,14 @@ zvalue_functions = { # there might be a better way of doing this... not really s
 
 class Variable(object) :
     """ Variable class to insulte from MCVariable becoming obsolete once we move away from trees!"""
-    def __init__( self, short_name, long_name, constraint_type = None, limit_value = 0, errors = [] ) :
-        if constraint_type is not None :
-            assert constraint_type in ["GAUSS", "UL", "LL"], \
-                "Unknown constraint type specificed"
-            assert len(errors) > 0, "No errors provided for constraint"
-
-            self.errors = errors
-            sq_errors = [ error**2 for error in errors ]
-            self.error = sqrt( sum(sq_errors) )
+    def __init__( self, short_name, long_name ) :
 
         self.short_name = short_name
         self.long_name = long_name
-        self.constraint_type = constraint_type
-        self.limit_value = limit_value
 
     def __str__( self ) :
         return self.short_name
 
-    def __repr__( self ) :
-        r_f = "%s [%s] %0.4f +- %0.4f" 
-        return r_f % ( self.short_name, self.constraint_type, self.limit_value, getattr(self,"error",0.) )
-
-    def getZValue(self, value) :
-        return zvalue_functions[self.constraint_type]( self, value )
-
-    def getChi2(self, value) :
-        return self.getZValue(value)**2
 
 class MCVariable(Variable, object) :
     def __init__( self, var, offset_relative_to = None, index_offset = 0 ) :
