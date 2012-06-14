0101620ae48e75c6376369ff5dccdf81bea809dd
Keesjan de Vries <keesjan777@gmail.com>
2012-06-14 15:27:45 +0100

    all files unticked, I think is a good default

--- config/files.py
+++ config/files.py
@@ -2,8 +2,11 @@
 import modules.file_dicts as files
 
 def recalc_files() :
+# COMBINATION OF ALL SAMPLING, APPLYING X^2 CUT
+#    d = files.cmssm_combine_sampling_input()
+#    d = files.nuhm1_combine_sampling_input()
 # TEST
-    d = files.cmssm_test_input_files()
+#    d = files.cmssm_test_input_files()
 #    d = files.nuhm1_test_input()
 #    d = files.nuhm1_test_input_files()
 # NUHM1
@@ -14,6 +17,7 @@ def recalc_files() :
 #    d = files.nuhm1_mc8_drop_bsmm_input()
 #    d = files.nuhm1_mc8_all_input()
 #CMSSM
+#    d = files.cmssm_mcmh_mc7_input()
 #    d = files.cmssm_mcmh_mh125_input()
 #    d = files.cmssm_mc8_drop_atlas_input()
 #    d = files.cmssm_mc8_drop_bsmm_input()
@@ -22,25 +26,29 @@ def recalc_files() :
 
 def histo_files() :
 # TEST
-    d = files.cmssm_test_file_histo()
+#    d = files.cmssm_test_file_histo()
 #    d = files.nuhm1_test_file_histo()
 #    d = files.nuhm1_test_histo()
 # NUHM1
+#    d = files.nuhm1_pre_lhc_histo()
 #    d = files.nuhm1_mcmh_mc7_histo()
 #    d = files.nuhm1_mcmh_mh125_histo()
-#    d = files.nuhm1_bsmm2012_histo()
 #    d = files.nuhm1_mc8_all_histo()
 #    d = files.nuhm1_mc8_drop_atlas_histo()
 #    d = files.nuhm1_mc8_drop_bsmm_histo()
 #CMSSM
+#    d = files.cmssm_pre_lhc_histo()
+#    d = files.cmssm_mcmh_mc7_histo()
 #    d = files.cmssm_mcmh_mh125_histo()
 #    d = files.cmssm_mc8_all_histo()
+#    d = files.cmssm_mc8_drop_atlas_histo()
+#    d = files.cmssm_mc8_drop_bsmm_histo()
     return d
 
 
 def point_files() :
 # TEST
-    d = files.cmssm_test_file_histo()
+#    d = files.cmssm_test_file_histo()
 #    d = files.nuhm1_test_file_histo()
 #    d = files.nuhm1_test_histo()
 # NUHM1
@@ -51,7 +59,10 @@ def point_files() :
 #    d = files.nuhm1_mc8_drop_atlas_histo()
 #    d = files.nuhm1_mc8_drop_bsmm_histo()
 #CMSSM
+#    d = files.cmssm_mcmh_mc7_histo()
 #    d = files.cmssm_mcmh_mh125_histo()
 #    d = files.cmssm_mc8_all_histo()
+#    d = files.cmssm_mc8_drop_atlas_histo()
+#    d = files.cmssm_mc8_drop_bsmm_histo()
     return d
 
