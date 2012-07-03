488090d7156e5039609ff5ca831b7aa5c4e404c9
Keesjan de Vries <keesjan777@gmail.com>
2012-07-03 22:12:29 +0100

    whatever

--- config/plots.py
+++ config/plots.py
@@ -2,10 +2,10 @@
 def plots_to_make() :
     d = {
 #            # spaces
-            ("m0", "m12")            : { "ranges" : [ (0,4000), (0, 3000) ],    "nbins" : [100,100] },
+           ("m0", "m12")            : { "ranges" : [ (0,4000), (0, 3000) ],    "nbins" : [100,100] },
             ("m0", "tanb")           : { "ranges" : [ (0,4000), (0, 60)   ],    "nbins" : [100,100] },
             ("tanb", "m12")          : { "ranges" : [ (0,60),   (0,3000)  ],    "nbins" : [100,100] },
-            ("mA0", "tanb")          : { "ranges" : [ (0,3000), (0,60)    ],    "nbins" : [100,100] },
+           ("mA0", "tanb")          : { "ranges" : [ (0,3000), (0,60)    ],    "nbins" : [100,100] },
           # ("neu1", "sigma_pp^SI")  : { "ranges" : [ (1,1000), (1E-12,1E-4) ], "nbins" : [100,100],"logaxes": [True,True] },
 #            ("neu1", "sigma_pp^SI_cm-2")  : { "ranges" : [ (1,1000), (1E-48,1E-40) ], "nbins" : [100,100],"logaxes": [True,True] },
           # splines
@@ -17,7 +17,7 @@ def plots_to_make() :
             ("squark_r",) : { "ranges" : [ (0,6000) ],  "nbins" : [100] },
             ("Bsmumu",)   : { "ranges" : [ (0,10.38E-9) ], "nbins" : [100] },
  #           ("BsmumuRatio",)   : { "ranges" : [ (0,3) ], "nbins" : [100] },
-      }
+     }
     return d
 
 def contributions_to_make() :
@@ -26,6 +26,7 @@ def contributions_to_make() :
     return l
 
 def predictions_to_make() :
-    l = []
+#    l = []
+    l =[ "mA0","tamb"]
 #    l = ["Bsmumu","m_h^0"]
     return l
