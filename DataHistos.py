340023bb7422d93f81f6b8030d56350297fa6e63
Keesjan de Vries <keesjan777@gmail.com>
2012-07-21 18:03:44 +0100

    part of cleanup

--- DataHistos.py
+++ DataHistos.py
@@ -13,13 +13,13 @@ def main( argv=None ) :
         contrib_vars = plots.get_contribs(mcf)
         predict_vars = plots.get_predicts(mcf)
         #plot_hist_KOhack_s = histogram_processing.get_entry_hist_KOhack_list(mcf, plot_vars)
-        entry_hist_KOhack_s = histogram_processing.get_entry_hist_KOhack_list(mcf, spaces   )
+        entry_hists = histogram_processing.get_entry_hists(mcf, spaces   )
 
         plot_modes = [ "pval", "chi2", "dchi" ]
 #        histogram_processing.fill_and_save_data_hists( mcf, plot_modes,
 #                                                       plot_hist_KOhack_s,
 #                                                       contrib_vars, predict_vars)
-        histogram_processing.fill_and_save_data_hists( mcf, spaces,entry_hist_KOhack_s,
+        histogram_processing.fill_and_save_data_hists( mcf, spaces,entry_hists,
                                                        plot_modes,contrib_vars, predict_vars)
 
 if __name__ == "__main__":
