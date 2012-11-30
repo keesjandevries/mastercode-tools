8f872ad1a8a95b9882ea7fc5cfd03ebb4a72ca86
Keesjan de Vries <keesjan777@gmail.com>
2012-11-30 15:02:02 +0000

    work in progress

--- modules/n_dim_entries.py
+++ modules/n_dim_entries.py
@@ -1,6 +1,26 @@
 #! /usr/bin/env python
 import ROOT as r
 from modules.mcchain import MCAnalysisChain
+from modules import variables 
+
+def fill_n_dim_hisogram(chain,n_dim_histo_list,space_dict,mcf):
+    
+    mc_vars = variables.mc_variables() # this gets all mc-variabls
+    space_vars = [  [mc_vars[sn] for sn in short_names  ] for short_names in space_dict  ]
+
+    begin   = 0
+#    end     = chain.GetEntries()
+    end     = 10000 
+    prog = ProgressBar(begin, end, 77, mode='fixed', char='#')
+    for entry in xrange(begin,end) :
+        chi2 = get_chi2(chain,mcf)
+        for histo, vars  in zip(n_dim_histo_list, space_vars )  :
+            coord=get_and_coordinates(vars,chain,mcf)
+            fill_histo(entry,chi2,coord)
+
+
+           
+        
 
 def init_interv_dict_list(space_dict):
     rdl=[]
@@ -10,7 +30,7 @@ def init_interv_dict_list(space_dict):
         nbins=space_dict[space]["nbins"]
         interv_list  = add_intervs_recursively([[]],0,intervs,nbins)
         for interv in interv_list:
-            dl[interv]=(-1 , 1.0e9) # will evenually be filled with bin number and X^2
+            dl[tuple(interv)]=(-1 , 1.0e9) # will evenually be filled with bin number and X^2
         rdl.append(dl)
     return rdl
 
@@ -38,9 +58,9 @@ def add_intervs_recursively(l_old, step, intervs, nbins):
 def go(mcf,space_dict):
 #   the space dict looks like this:
 #   { ("m0", "m12","A0","tanb") : { "ranges" : [ (0,4000), (0, 4000), (-5000,5000),(2,68) ],   "nbins" : [3,3,3,3] },... }
-#    chain = MCAnalysisChain(mcf)
+    chain = MCAnalysisChain(mcf)
     print space_dict
     interv_dict_list = init_interv_dict_list(space_dict)
-    print interv_dict_list
+    fill_n_dim_hisogram(chain,interv_dict_list,space_dict,mcf)
     
     return interv_dict_list
